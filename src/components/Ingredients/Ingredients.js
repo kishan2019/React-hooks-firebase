00399b01c10c46408b6a563ec64f30a46acb97e3
kishan singh <kishan.singh@mountblue.io>
2020-01-23 01:41:47 +0530

    added Error handling.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,31 +1,33 @@
-import React, { useState, useEffect,useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 
 const Ingredients = () => {
   const [userIngredient, setUserIngrediant] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filterIngredientHandler = useCallback(filterIngredients => {
     setUserIngrediant(filterIngredients);
-  },[]);
+  }, []);
 
   useEffect(() => {
     fetch('https://react-hooks-update-459d5.firebaseio.com/ingredients.json')
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredient = [];
-      for(let key in responseData){
-        loadedIngredient.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
-      }
-      setUserIngrediant(loadedIngredient);
-    });
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredient = [];
+        for (let key in responseData) {
+          loadedIngredient.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngrediant(loadedIngredient);
+      });
   }, []);
 
   const addIngredientHandler = ingredient => {
@@ -39,28 +41,37 @@ const Ingredients = () => {
       return response.json();
     }).then(responseData => {
       setUserIngrediant(prevIngredient => [
-      ...prevIngredient,
-      { id: responseData.name, ...ingredient }
+        ...prevIngredient,
+        { id: responseData.name, ...ingredient }
       ]);
     });
   };
 
   const removeIngredientHandler = ingredientId => {
     setIsLoading(true)
-    fetch(`https://react-hooks-update-459d5.firebaseio.com/ingredients/${ingredientId}.json`,{
+    fetch(`https://react-hooks-update-459d5.firebaseio.com/ingredients/${ingredientId}.on`, {
       method: 'DELETE'
     })
-    .then(response => {
-      setIsLoading(false)
-      setUserIngrediant(prevIngredients =>
-        prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-      );
-    });
+      .then(response => {
+        setIsLoading(false)
+        setUserIngrediant(prevIngredients =>
+          prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+        );
+      })
+      .catch(error => {
+        setError(error.message)
+      });
   };
+
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
+  }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
         <Search onLoadIngredients={filterIngredientHandler} />
@@ -68,7 +79,7 @@ const Ingredients = () => {
       </section>
     </div>
   );
- 
+
 };
 
 export default Ingredients;
